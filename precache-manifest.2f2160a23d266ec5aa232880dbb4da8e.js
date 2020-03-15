4c0fc8434eaeaabcc1d26314f40837fcccdd20b0
bdesignb <bozana.bundalo@gmail.com>
2020-03-15 10:55:26 +0100

    Updates

---
+++ precache-manifest.2f2160a23d266ec5aa232880dbb4da8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b207cb9df7b7141c42b40bb5b80dfc9",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "f9123fb9cde823991543",
+    "url": "/Portfolio-Website/static/css/main.8743babe.chunk.css"
+  },
+  {
+    "revision": "ebfb47629c5e724fbf23",
+    "url": "/Portfolio-Website/static/js/2.55c6397b.chunk.js"
+  },
+  {
+    "revision": "f9123fb9cde823991543",
+    "url": "/Portfolio-Website/static/js/main.c5c6ca74.chunk.js"
+  },
+  {
+    "revision": "668fe7d7cecfb2c997b5",
+    "url": "/Portfolio-Website/static/js/runtime-main.47b76ea3.js"
+  },
+  {
+    "revision": "86d9973a9dea854f3b07c1adca2e59d3",
+    "url": "/Portfolio-Website/static/media/header.86d9973a.jpg"
+  }
+]);
